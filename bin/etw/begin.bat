3f056c7ed73ac2acfb5631bcd899e2fa6505e690
vinjn <vinjn.z@gmail.com>
2022-08-03 22:03:58 +0800

    etw, support gpu tracing

--- bin/etw/begin.bat
+++ bin/etw/begin.bat
@@ -1,3 +1,18 @@
-@del %LocalAppData%\UIForETWkernel.etl
-@del %LocalAppData%\UIForETWuser.etl
-xperf.exe -start "NT Kernel Logger" -on Latency+POWER+DISPATCHER+DISK_IO_INIT+FILE_IO+FILE_IO_INIT+VIRT_ALLOC+MEMINFO -stackwalk Profile+CSwitch+ReadyThread -buffersize 1024 -minbuffers 1200 -maxbuffers 1200 -f "%LocalAppData%\UIForETWkernel.etl" -start UIforETWSession -on Microsoft-Windows-Win32k:0xfdffffffefffffff+Multi-MAIN+Multi-FrameRate+Multi-Input+Multi-Worker+Microsoft-Windows-Kernel-Memory:0xE0+Microsoft-Windows-Kernel-Process+Microsoft-Windows-Kernel-Power+Microsoft-Windows-DxgKrnl:0xFFFF:5+Microsoft-Windows-MediaEngine -buffersize 1024 -minbuffers 400 -maxbuffers 400 -f "%LocalAppData%\UIForETWuser.etl"
+@set kernelfile=%LocalAppData%\kernel.etl
+@set userfile=%LocalAppData%\user.etl
+
+@set KernelProviders=PROC_THREAD+LOADER+Latency+POWER+DISPATCHER+DISK_IO_INIT+FILE_IO+FILE_IO_INIT+VIRT_ALLOC+MEMINFO
+
+@set SessionName=UIforETWSession
+@set TRACE_DXC=802ec45a-1e99-4b83-9920-87c98277ba9d
+@SET SCHEDULER_LOG_STATE=%TRACE_DXC%:0x04000000:5
+@set DX_Flags=Microsoft-Windows-DxgKrnl:0xFFFF:5+Microsoft-Windows-MediaEngine
+@set UserProviders=%DX_Flags%+Microsoft-Windows-Win32k:0xfdffffffefffffff+Multi-MAIN+Multi-FrameRate+Multi-Input+Multi-Worker+Microsoft-Windows-Kernel-Memory:0xE0+Microsoft-Windows-Kernel-Process
+
+xperf.exe ^
+ -start "NT Kernel Logger" -on %KernelProviders% ^
+ -stackwalk Profile+CSwitch+ReadyThread -buffersize 1024 -minbuffers 1200 -maxbuffers 1200 ^
+ -f %kernelfile% ^
+ -start %SessionName% -on %UserProviders% -buffersize 1024 -minbuffers 400 -maxbuffers 400 ^
+ -f %userfile%
+xperf.exe -capturestate %SessionName% %UserProviders%
--- bin/etw/end.bat
+++ bin/etw/end.bat
@@ -1,8 +1,11 @@
+@setlocal
+
 @set dt=%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
 @set TIMESTAMP=%dt: =0%
+@set TIMESTAMP=%DATE:/=-% %TIME::=-%
 xperf -stop "NT Kernel Logger"
-xperf -stop UIforETWSession
+xperf -stop %SessionName%
 @rem xperf -stop
-xperf -merge %LocalAppData%\UIForETWuser.etl %LocalAppData%\UIForETWkernel.etl GpuProf-%TIMESTAMP%.etl
-@del %LocalAppData%\UIForETWuser.etl
-@del %LocalAppData%\UIForETWkernel.etl
+xperf -merge "%kernelfile%" "%userfile%" "%~dp0GpuProf-%TIMESTAMP%.etl" -compress
+@del %kernelfile%
+@del %userfile%
